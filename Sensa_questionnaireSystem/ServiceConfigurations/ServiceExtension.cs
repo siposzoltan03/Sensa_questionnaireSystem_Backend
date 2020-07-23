db57113cf765f88e61eaec7551935554625d4e14
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-07-23 15:56:09 +0200

    Add service conf extension file

---
+++ Sensa_questionnaireSystem/ServiceConfigurations/ServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Sensa_questionnaireSystem.Logic.Helpers;
+
+namespace Sensa_questionnaireSystem.ServiceConfigurations
+{
+    public static class ServiceExtension
+    {
+        public static void AddScopes(this IServiceCollection service)
+        {
+            service.AddScoped<HomeHelperFunctions>();
+        }
+    }
+}
