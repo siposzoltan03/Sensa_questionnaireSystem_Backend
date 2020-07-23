dd161126d549141718a99a60dc03f8991c617ad5
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-07-23 15:57:27 +0200

    Refactor route logic in separate helper function

---
+++ Sensa_questionnaireSystem/Logic/Helpers/HomeHelperFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Sensa_questionnaireSystem.Model;
+
+namespace Sensa_questionnaireSystem.Logic.Helpers
+{
+    public class HomeHelperFunctions
+    {
+        public HomeHelperFunctions()
+        {
+        }
+
+        public List<Questionnaires> FilterActiveQuestionnaires(List<Questionnaires> questionnaires)
+        {
+            var result = questionnaires
+                .Where(questionnaire => questionnaire.State == "a" || questionnaire.State == "i");
+            return result.ToList();
+        }
+    }
+}
