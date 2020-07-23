52ee64c6a34c251d3797488d817596d561ac8a8d
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-07-23 22:55:01 +0200

    Add routes to QuestionnaireTemplateGroupController

---
+++ Sensa_questionnaireSystem/Controllers/QuestionnaireTemplateGroupController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sensa_questionnaireSystem.Context;
+using Sensa_questionnaireSystem.Model;
+
+namespace Sensa_questionnaireSystem.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class QuestionnaireTemplateGroupController : Controller
+    {
+        private readonly QuestionnaireSystemContext _context;
+
+        public QuestionnaireTemplateGroupController(QuestionnaireSystemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Questiongroups>>> GetQuestionGroups()
+        {
+            var result = await _context.Questiongroups.ToListAsync();
+            if (result == null) return NotFound();
+
+            return new ActionResult<IEnumerable<Questiongroups>>(result.Take(10));
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Questions>>> GetQuestions()
+        {
+            var result = await _context.Questions.ToListAsync();
+            if (result == null) return NotFound();
+
+            return result;
+        }
+    }
+}
